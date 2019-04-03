b411889942e87dc31e1336a7a067762a625f1164
Manzanit0 <unico.javier@gmail.com>
2019-04-03 20:15:37 +0100

    Adds test case: Two fluxes can be combined.

--- src/test/java/ReactorCoreTests.java
+++ src/test/java/ReactorCoreTests.java
@@ -46,4 +46,21 @@ public class ReactorCoreTests {
         assertThat(doubledElements).containsExactly(2, 4, 6, 8);
         assertThat(incrementedElements).containsExactly(3, 5, 7, 9);
     }
+
+    @Test
+    public void twoFluxesCanBeCombined() {
+        List<String> elements = new ArrayList<>();
+
+        Flux.just(1, 2, 3, 4)
+                .map(i -> i * 2)
+                .zipWith(Flux.range(0, Integer.MAX_VALUE),
+                        (one, two) -> String.format("First Flux: %d, Second Flux: %d", one, two))
+                .subscribe(elements::add);
+
+        assertThat(elements).containsExactly(
+                "First Flux: 2, Second Flux: 0",
+                "First Flux: 4, Second Flux: 1",
+                "First Flux: 6, Second Flux: 2",
+                "First Flux: 8, Second Flux: 3");
+    }
 }
